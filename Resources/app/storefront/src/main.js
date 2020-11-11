44d9dde01e00fa3af1193f411815bd7fd20d9cbb
Jonas Elfering <j.elfering@shopware.com>
2020-11-11 14:20:50 +0100

    Add banner

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,4 +1,6 @@
 import ConsentManagerPlugin from './js/consent-manager.plugin';
+import HideOnClickPlugin from './plugin/hide-on-click/hide-on-click.plugin';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('ConsentManagerPlugin', ConsentManagerPlugin, '[data-consent-manager-plugin]');
+PluginManager.register('HideOnClickPlugin', HideOnClickPlugin, '[data-hide-on-click-container]');
