b88d261a6a7b2661d4cad0d0ca77c34f7ad65c9a
Jonas Elfering <j.elfering@shopware.com>
2020-11-13 11:39:09 +0100

    Add newsletter popup

--- Resources/app/storefront/src/main.js
+++ Resources/app/storefront/src/main.js
@@ -1,8 +1,10 @@
 import ConsentManagerPlugin from './js/consent-manager.plugin';
-import HideOnClickPlugin from './plugin/hide-on-click/hide-on-click.plugin';
 import InfoModalPlugin from './js/info-modal.plugin';
+import HideOnClickPlugin from './js/hide-on-click.plugin';
+import ModalOnLoadPlugin from './js/modal-on-load.plugin';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('ConsentManagerPlugin', ConsentManagerPlugin, '[data-consent-manager-plugin]');
 PluginManager.register('HideOnClickPlugin', HideOnClickPlugin, '[data-hide-on-click-container]');
 PluginManager.register('InfoModalPlugin', InfoModalPlugin, '[data-info-modal-plugin]');
+PluginManager.register('ModalOnLoadPlugin', ModalOnLoadPlugin, '[data-open-modal-on-load]');
