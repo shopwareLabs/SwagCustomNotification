005a400f79a69b317fdd4f3b537911fa47429db4
Jonas Elfering <j.elfering@shopware.com>
2021-01-22 11:14:07 +0100

    SAAS-1828 - Use js pseudo modal

--- Resources/app/storefront/src/js/modal-on-load.plugin.js
+++
@@ -1,9 +0,0 @@
-import Plugin from 'src/plugin-system/plugin.class';
-
-export default class ModalOnLoadPlugin extends Plugin {
-
-    init() {
-        const _modal = this.el;
-        $(_modal).modal('show');
-    }
-}
