95079225f8abcb8a97cf1fd57ce0ac523b2bf033
Jonas Elfering <j.elfering@shopware.com>
2021-01-28 14:46:05 +0100

    SAAS-1828 - Fix consent with http cache

--- Resources/app/storefront/src/js/consent-manager.plugin.js
+++ Resources/app/storefront/src/js/consent-manager.plugin.js
@@ -1,5 +1,4 @@
 import Plugin from 'src/plugin-system/plugin.class';
-import CookieStorage from 'src/helper/storage/cookie-storage.helper';
 import DomAccess from 'src/helper/dom-access.helper';
 
 export default class ConsentManagerPlugin extends Plugin {
@@ -7,8 +6,8 @@ export default class ConsentManagerPlugin extends Plugin {
   static options = {
     selector_btn_consent: '.swag-custom-notification-consent-modal__consent-button',
     class_no_scroll: 'no-scroll',
-    cookie_key: 'sw-consent-manager-accepted',
-    cookie_lifetime_days: 90,
+    storage_key: 'sw-consent-manager-accepted',
+    storage_lifetime_days: 90,
   };
 
   init() {
@@ -17,11 +16,16 @@ export default class ConsentManagerPlugin extends Plugin {
 
 
   initConsentModal() {
-    const { class_no_scroll, selector_btn_consent } = this.options;
+    const { storage_key, class_no_scroll, selector_btn_consent } = this.options;
 
-    DomAccess.querySelector(document, 'html').classList.add(class_no_scroll);
-    this.buttonConsent = DomAccess.querySelector(this.el, selector_btn_consent);
-    this.buttonConsent.addEventListener('click', this.handleClick.bind(this));
+    if (window.localStorage.getItem(storage_key)) {
+        this.el.style.display = 'none';
+    } else {
+        this.el.style.display = 'block';
+        DomAccess.querySelector(document, 'html').classList.add(class_no_scroll);
+        this.buttonConsent = DomAccess.querySelector(this.el, selector_btn_consent);
+        this.buttonConsent.addEventListener('click', this.handleClick.bind(this));
+    }
   }
 
   handleClick(event) {
@@ -29,18 +33,13 @@ export default class ConsentManagerPlugin extends Plugin {
     event.stopPropagation();
 
     this.saveConsent();
-    this.reloadContent();
+    this.init();
   }
 
   saveConsent() {
-    const { cookie_key, cookie_lifetime_days, class_no_scroll } = this.options;
+    const { storage_key, storage_lifetime_days, class_no_scroll } = this.options;
 
-    CookieStorage.setItem(cookie_key, '1', cookie_lifetime_days);
+    window.localStorage.setItem(storage_key, '1', storage_lifetime_days);
     DomAccess.querySelector(document, 'html').classList.remove(class_no_scroll);
-  }
-
-  reloadContent() {
-    this.buttonConsent.disabled = true;
-    window.location.reload();
   }
 }
